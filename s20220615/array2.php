d179d02cf9500ac00702641f954adeba15338330
kai <ckk0910@gmail.com>
2022-06-15 14:37:55 +0800

    object reduce

--- s20220615/array2.php
+++ s20220615/array2.php
@@ -17,19 +17,18 @@
     foreach ($myArr as $key => $value) {
         print_r($key);
         print_r($value);
-
-        // php 多維陣列
-
-        $students = [
-            [
-                'id' => 1,
-                'name' => 'amy',
-                'tel' => '0911-111-111'
-            ],
-            [
-                'id' => 2,
-                'name' => 'bob',
-                'tel' => '0922-222-222'
-            ],
-        ];
     }
+
+    // php 多維陣列
+    $students = [
+        [
+            'id' => 1,
+            'name' => 'amy',
+            'tel' => '0911-111-111'
+        ],
+        [
+            'id' => 2,
+            'name' => 'bob',
+            'tel' => '0922-222-222'
+        ],
+    ];
